ca33611767d535b648d2bf69767b37143a6c452a
江峰 <jiangfeng@smarchit.com>
2019-11-27 16:47:03 +0800

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
  * @author 江峰
  * @create 2019-11-27   16:43
  */
-public class Hello {
+public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
     }
