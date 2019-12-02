33e851d9dfb4cedb6d2ea293f04aa481c2dd2ae7
江峰 <1546844168@qq.com>
2019-12-02 22:45:23 +0800

    lock和condition实现做一道菜上一道菜

---
+++ src/condition/MakeFood.java
@@ -0,0 +1,56 @@
+package condition;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author 江峰
+ * @create 2019-12-02   22:04
+ */
+
+
+public class MakeFood {
+    public static void main(String[] args) throws InterruptedException {
+        final ReentrantLock lock = new ReentrantLock();
+        Condition c1 = lock.newCondition();
+        Condition c2 = lock.newCondition();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                lock.lock();
+                try {
+                    while (true) {
+                        c2.await();
+                        System.out.println(Thread.currentThread().getName() + "-------菜上好了");
+                        TimeUnit.SECONDS.sleep(1);
+                        c1.signalAll();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }).start();
+        TimeUnit.MILLISECONDS.sleep(10);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                lock.lock();
+                try {
+                    while (true) {
+                        System.out.println(Thread.currentThread().getName() + "菜做好了");
+                        TimeUnit.SECONDS.sleep(1);
+                        c2.signalAll();
+                        c1.await();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }).start();
+    }
+}
