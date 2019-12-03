94a0512d25c4ce4cdee1b1ab7b9f686de4e7b5a6
江峰 <1546844168@qq.com>
2019-12-03 16:16:09 +0800

    饿汉式

---
+++ src/singleton/Singleton2.java
@@ -0,0 +1,19 @@
+package singleton;
+
+/**
+ * 饿汉式
+ * 线程不安全
+ *
+ * @author 江峰
+ * @create 2019-12-03   16:04
+ */
+public class Singleton2 {
+    private static Singleton2 singleton = null;
+
+    public singleton.Singleton2 getSingleton() {
+        if (singleton == null) {
+            singleton = new Singleton2();
+        }
+        return singleton;
+    }
+}
