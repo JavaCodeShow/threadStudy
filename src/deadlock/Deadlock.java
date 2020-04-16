1dd4ab7df6219e4c5f4881b318dfe1c0267e026f
江峰 <1546844168@qq.com>
2020-04-16 15:42:16 +0800

    学习

---
+++ src/deadlock/Deadlock.java
@@ -0,0 +1,49 @@
+package deadlock;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author 江峰
+ * @create 2019-12-04   13:52
+ */
+public class Deadlock {
+    public static void main(String[] args) throws InterruptedException {
+        final Object a = new Object();
+        final Object b = new Object();
+        Thread threadA = new Thread(new Runnable() {
+            public void run() {
+                synchronized (a) {
+                    try {
+                        System.out.println("now i in threadA-locka");
+                        TimeUnit.SECONDS.sleep(1);
+                        synchronized (b) {
+                            System.out.println("now i in threadA-lockb");
+                        }
+                    } catch (Exception e) {
+                        // ignore
+                    }
+                }
+            }
+        });
+
+        Thread threadB = new Thread(new Runnable() {
+            public void run() {
+                synchronized (b) {
+                    try {
+                        System.out.println("now i in threadB-lockb");
+                        TimeUnit.SECONDS.sleep(1);
+                        synchronized (a) {
+                            System.out.println("now i in threadB-locka");
+                        }
+                    } catch (Exception e) {
+                        // ignore
+                    }
+                }
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+    }
+
+}
---
+++ src/deadlock/ThreadPoolDeadLock.java
@@ -0,0 +1,35 @@
+package deadlock;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * @author 江峰
+ * @create 2019-12-04   14:45
+ */
+public class ThreadPoolDeadLock {
+    public static void main(String[] args) {
+        final ExecutorService executorService =
+                Executors.newFixedThreadPool(2);
+        Future<Long> f1 = executorService.submit(new Callable<Long>() {
+
+            public Long call() throws Exception {
+                System.out.println("start f1");
+                Thread.sleep(1000);//延时
+                Future<Long> f2 =
+                        executorService.submit(new Callable<Long>() {
+
+                            public Long call() throws Exception {
+                                System.out.println("start f2");
+                                return -1L;
+                            }
+                        });
+                System.out.println("result" + f2.get());
+                System.out.println("end f1");
+                return -1L;
+            }
+        });
+    }
+}
