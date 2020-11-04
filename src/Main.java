85daabcd91b51a9ba703f16ee16a40bc3f604dc8
江峰 <1546844168@qq.com>
2020-11-04 22:19:53 +0800

    死锁

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -7,42 +6,23 @@ import java.util.concurrent.locks.ReentrantLock;
  * @create 2019-11-27   16:43
  */
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         final Lock l1 = new ReentrantLock();
         final Lock l2 = new ReentrantLock();
-        Thread threadA = new Thread(new Runnable() {
-            public void run() {
-                l1.lock();
-                try {
-                    System.out.println("now i in threadA-locka");
-                    Thread.sleep(1000l);
-                    l2.lock();
-                    System.out.println("now i in threadA-lockb");
-                } catch (Exception e) {
-                    // ignore
-                }
+        Thread threadA = new Thread(() -> {
+            l1.lock();
+            try {
+                System.out.println("now i in threadA-locka");
+                Thread.sleep(1000L);
+                l2.lock();
+                System.out.println("now i in threadA-lockb");
+            } catch (Exception e) {
+                // ignore
             }
         });
+    }
 
-        Thread threadB = new Thread(new Runnable() {
-            public void run() {
-                try {
-                    l2.lock();
-                    System.out.println("now i in threadB-lockb");
-                    Thread.sleep(1000l);
-                    l1.tryLock(1, TimeUnit.SECONDS);
-                    System.out.println("now i in threadB-locka");
-                } catch (Exception e) {
-                    // ignore
-                }
-            }
-        });
-
-        threadA.start();
-        threadB.start();
-        // TimeUnit.SECONDS.sleep(2);
-        // threadA.interrupt();
-        // boolean interrupted = threadA.isInterrupted();
-        // System.out.println(interrupted);
+    public synchronized void hello() {
+        System.out.println("synchronized 代码块");
     }
 }
--- src/deadlock/Deadlock.java
+++ src/deadlock/Deadlock.java
@@ -7,37 +7,33 @@ import java.util.concurrent.TimeUnit;
  * @create 2019-12-04   13:52
  */
 public class Deadlock {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         final Object a = new Object();
         final Object b = new Object();
-        Thread threadA = new Thread(new Runnable() {
-            public void run() {
-                synchronized (a) {
-                    try {
-                        System.out.println("now i in threadA-locka");
-                        TimeUnit.SECONDS.sleep(1);
-                        synchronized (b) {
-                            System.out.println("now i in threadA-lockb");
-                        }
-                    } catch (Exception e) {
-                        // ignore
+        Thread threadA = new Thread(() -> {
+            synchronized (a) {
+                try {
+                    System.out.println("now i in threadA-locka");
+                    TimeUnit.SECONDS.sleep(1);
+                    synchronized (b) {
+                        System.out.println("now i in threadA-lockb");
                     }
+                } catch (Exception e) {
+                    // ignore
                 }
             }
         });
 
-        Thread threadB = new Thread(new Runnable() {
-            public void run() {
-                synchronized (b) {
-                    try {
-                        System.out.println("now i in threadB-lockb");
-                        TimeUnit.SECONDS.sleep(1);
-                        synchronized (a) {
-                            System.out.println("now i in threadB-locka");
-                        }
-                    } catch (Exception e) {
-                        // ignore
+        Thread threadB = new Thread(() -> {
+            synchronized (b) {
+                try {
+                    System.out.println("now i in threadB-lockb");
+                    TimeUnit.SECONDS.sleep(1);
+                    synchronized (a) {
+                        System.out.println("now i in threadB-locka");
                     }
+                } catch (Exception e) {
+                    // ignore
                 }
             }
         });
