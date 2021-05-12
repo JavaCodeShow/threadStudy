c613ab4685e11037aca98716d9c5b8ce41060da9
江峰 <feng.jiang@marketin.cn>
2021-05-12 22:27:40 +0800

    format code

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,11 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
-
 /**
  * @author 江峰
  * @create 2019-11-27 16:43
  */
 
 public class Main {
+
 	public static void main(String[] args) throws InterruptedException {
-		List<String> names = Arrays.asList("Larry", "Steve", "James");
-		AtomicReference<String> str = new AtomicReference<>();
-		names.forEach(s -> {
-			if ("Steve".equals(s)) {
-				str.set(s);
-			}
-		});
-		String s = str.get();
-		System.out.println(s);
+
 	}
 }
--- src/condition/LockAndCondition.java
+++ src/condition/LockAndCondition.java
@@ -16,45 +16,34 @@ public class LockAndCondition {
 		// condition是一个实现多线程间通信的工具类。
 		Condition condition = lock.newCondition();
 		ExecutorService es = Executors.newFixedThreadPool(2);
-		es.execute(new Runnable() {
-			@Override
-			public void run() {
-				lock.lock();
-				try {
-					System.out.println(
-							Thread.currentThread().getName() + "我获取到锁了");
-					System.out.println(
-							Thread.currentThread().getName() + "我开始等待");
-					condition.await();
-					System.out.println(
-							Thread.currentThread().getName() + "我等待结束了");
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				} finally {
-					lock.unlock();
-					System.out.println(
-							Thread.currentThread().getName() + "我释放锁了");
-				}
+		es.execute(() -> {
+			lock.lock();
+			try {
+				System.out.println(Thread.currentThread().getName() + "我获取到锁了");
+				System.out.println(Thread.currentThread().getName() + "我开始等待");
+				condition.await();
+				System.out.println(Thread.currentThread().getName() + "我等待结束了");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				lock.unlock();
+				System.out.println(Thread.currentThread().getName() + "我释放锁了");
 			}
 		});
-		es.execute(new Runnable() {
-			@Override
-			public void run() {
-				lock.lock();
-				try {
-					System.out.println(
-							Thread.currentThread().getName() + "我拿到锁了");
-					TimeUnit.SECONDS.sleep(2);
-					condition.signal();
-					System.out.println(
-							Thread.currentThread().getName() + "我释放了一个信号");
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				} finally {
-					lock.unlock();
-					System.out.println(
-							Thread.currentThread().getName() + "我释放锁了");
-				}
+
+		es.execute(() -> {
+			lock.lock();
+			try {
+				System.out.println(Thread.currentThread().getName() + "我拿到锁了");
+				TimeUnit.SECONDS.sleep(2);
+				condition.signal();
+				System.out
+						.println(Thread.currentThread().getName() + "我释放了一个信号");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				lock.unlock();
+				System.out.println(Thread.currentThread().getName() + "我释放锁了");
 			}
 		});
 		es.shutdown();
