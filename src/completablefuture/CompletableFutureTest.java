96f31dd72eb05600e2a39af129c6da37e9410768
江峰 <jiangfeng@morefungroup.com>
2021-07-05 20:50:44 +0800

    代码优化

--- src/completablefuture/CompletableFutureTest.java
+++ src/completablefuture/CompletableFutureTest.java
@@ -9,74 +9,66 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class CompletableFutureTest {
-	public static CompletableFuture<String> findAccount(String accountId) {
-		return CompletableFuture.supplyAsync(() -> {
-			// mock finding account from database
-			try {
-				System.out.println(Thread.currentThread().getName() + ":start!"
-						+ "   时间：" + LocalTime.now());
-				TimeUnit.SECONDS.sleep(1);
-				System.out.println(Thread.currentThread().getName() + ":end!"
-						+ "   时间：" + LocalTime.now());
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			return "account" + accountId;
-		});
-	}
+    public static CompletableFuture<String> findAccount(String accountId) {
+        return CompletableFuture.supplyAsync(() -> {
+            // mock finding account from database
+            try {
+                System.out.println(Thread.currentThread().getName() + ":start!"
+                        + "   时间：" + LocalTime.now());
+                TimeUnit.SECONDS.sleep(1);
+                System.out.println(Thread.currentThread().getName() + ":end!"
+                        + "   时间：" + LocalTime.now());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "account" + accountId;
+        });
+    }
 
-	public static void batchProcess(List<String> accountIdList) {
-		// 并行根据accountId查找对应account
-		List<CompletableFuture<String>> accountFindingFutureList = accountIdList
-				.stream().map(accountId -> findAccount(accountId))
-				.collect(Collectors.toList());
+    public static void batchProcess(List<String> accountIdList) {
+        // 并行根据accountId查找对应account
+        List<CompletableFuture<String>> accountFindingFutureList = accountIdList
+                .stream().map(accountId -> findAccount(accountId))
+                .collect(Collectors.toList());
 
-		// 使用allOf方法来表示所有的并行任务
-		CompletableFuture<Void> allFutures = CompletableFuture
-				.allOf(accountFindingFutureList
-						.toArray(new CompletableFuture[accountFindingFutureList
-								.size()]));
+        // 使用allOf方法来表示所有的并行任务
+        CompletableFuture<Void> allFutures = CompletableFuture
+                .allOf(accountFindingFutureList
+                        .toArray(new CompletableFuture[accountFindingFutureList
+                                .size()]));
 
-		// 下面的方法可以帮助我们获得所有子任务的处理结果
-		CompletableFuture<List<String>> finalResults = allFutures
-				.thenApply(v -> accountFindingFutureList.stream().map(
-						accountFindingFuture -> accountFindingFuture.join())
-						.collect(Collectors.toList()));
+        // 下面的方法可以帮助我们获得所有子任务的处理结果
+        CompletableFuture<List<String>> finalResults = allFutures
+                .thenApply(v -> accountFindingFutureList.stream().map(
+                        accountFindingFuture -> accountFindingFuture.join())
+                        .collect(Collectors.toList()));
 
-		try {
-			List<String> l = finalResults.get();
-			System.out.println(l);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			e.printStackTrace();
-		}
-	}
+        try {
+            List<String> l = finalResults.get();
+            System.out.println(l);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static void main(String[] args)
-			throws ExecutionException, InterruptedException {
-		List<String> l = new ArrayList<String>();
-		l.add("11111");
-		l.add("22222");
-		l.add("33333");
-		// batchProcess(l);
-		List<CompletableFuture<String>> futureList = new ArrayList<>();
-		l.forEach(x -> {
-			futureList.add(CompletableFuture.supplyAsync(() -> {
-				// 这个就是实际的每一个任务的结果
-				return x + " : hello";
-			}));
-		});
-		// CompletableFuture<Void> voidCompletableFuture =
-		// CompletableFuture.allOf(futureList.toArray(new
-		// CompletableFuture[futureList.size()]));
-		// CompletableFuture<List<Object>> listCompletableFuture =
-		// voidCompletableFuture.thenApply(v ->
-		// futureList.stream().map(completableFuture ->
-		// completableFuture.join()).collect(Collectors.toList()));
-		// List<Object> objects = listCompletableFuture.get();
-		List<String> strings = new CompletableFutureUtils<String>()
-				.batchAsyncRunThenMergeResult(futureList);
-		System.out.println(strings);
-	}
+    public static void main(String[] args)
+            throws ExecutionException, InterruptedException {
+        List<String> l = new ArrayList<String>();
+        l.add("11111");
+        l.add("22222");
+        l.add("33333");
+        // batchProcess(l);
+        List<CompletableFuture<String>> futureList = new ArrayList<>();
+        l.forEach(x -> {
+            futureList.add(CompletableFuture.supplyAsync(() -> {
+                // 这个就是实际的每一个任务的结果
+                return x + " : hello";
+            }));
+        });
+
+        List<String> stringList = futureList.stream().map(CompletableFuture::join).collect(Collectors.toList());
+        System.out.println(stringList);
+    }
 }
